88a39f537d2a938c8d87c67cc6584454465f8fb1
Prikoliste <157858593+Prikoliste@users.noreply.github.com>
2025-02-09 00:12:11 +0000

    project core start

--- base/handlers/commands/start.py
+++ base/handlers/commands/start.py
@@ -1,15 +1,31 @@
 from aiogram import Router, types
 from aiogram.filters.command import Command
 from aiogram.fsm.state import State, StatesGroup
-
-
+from aiogram.fsm.context import FSMContext
 
 start_router = Router()
 
-
-
-class
+class StartStatesGroup(StatesGroup):
+    full_name_w8ing = State()
+    ph_number_w8ing = State()
 
 @start_router.message(Command('start'))
-async def start(message: types.Message):
-    await message.answer('start')
+async def start(message: types.Message, state: FSMContext):
+    await message.answer('Отправите Срочно ваше имя...')
+    await state.set_state(StartStatesGroup.full_name_w8ing)
+
+@start_router.message(StartStatesGroup.full_name_w8ing)
+async def save_name(message: types.Message, state: FSMContext):
+    await state.update_data(full_name=message.text)
+    await message.answer('Отправите Срочно ваше номер...')
+    await state.set_state(StartStatesGroup.ph_number_w8ing)
+
+@start_router.message(StartStatesGroup.ph_number_w8ing)
+async def save_number(message: types.Message, state: FSMContext):
+    await state.update_data(full_number=message.text)
+    await get_data()
+
+async def get_data(state: FSMContext, message: types.Message):
+    data = await state.get_data()
+    await message.answer(data['full_name']+data['ph_number'])
+    await state.clear()
